a228dcb99c2515502a0c7f95df8163702240992d
karan95 <kmthakor95@gmail.com>
2020-10-13 14:52:51 -0400

    added some integration tests

--- tests/integration/components/food-container-test.js
+++ tests/integration/components/food-container-test.js
@@ -1,26 +1,45 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, findAll } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | food-container', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders FoodContainer component', async function(assert) {
     await render(hbs`<FoodContainer />`);
+    assert.ok(findAll('.food-app-header').length == 1, 'renders header of food search engine');
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.ok(findAll('#search-input').length == 1, 'renders header of input box to search food');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <FoodContainer>
-        template block text
-      </FoodContainer>
-    `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('it renders Loading bar', async function(assert) {
+    this.set('loading', true);
+    await render(hbs`<FoodContainer @searchDisabled={{loading}}/>`);
+
+    assert.ok(findAll('.spinner-border').length == 1, 'renders food list container');
+  });
+
+  test('it renders FoodList component', async function(assert) {
+    const sample_data = [
+      {
+        food: {
+          label: 'data1',
+          foodContentsLabel: 'test',
+          categoryLabel: 'test'
+        }
+      },
+      {
+        food: {
+          label: 'data2',
+          foodContentsLabel: 'test',
+          categoryLabel: 'test'
+        }
+      }
+    ]
+    this.set('foodItemList', sample_data);
+    await render(hbs`<FoodContainer @foodItemList={{foodItemList}}/>`);
+    assert.ok(findAll('.food-list-container').length == 1, 'renders food list container');
   });
 });
--- tests/integration/components/food-list-test.js
+++ tests/integration/components/food-list-test.js
@@ -1,26 +1,30 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, findAll } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | food-list', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<FoodList />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <FoodList>
-        template block text
-      </FoodList>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('it renders foodList table component with search box', async function(assert) {
+    const sample_data = [
+      {
+        food: {
+          label: 'data1',
+          foodContentsLabel: 'test',
+          categoryLabel: 'test'
+        }
+      },
+      {
+        food: {
+          label: 'data2',
+          foodContentsLabel: 'test',
+          categoryLabel: 'test'
+        }
+      }
+    ]
+    this.set('foodItemList', sample_data);
+    await render(hbs`<FoodList @foodItemList={{foodItemList}}/>`);
+    assert.ok(findAll('.search-table').length == 1, 'renders input box in table');
   });
 });
